c00a46f66e115920b5a00f14f18ae9e534bec9f0
mike connor <mike@rocketnumber9.org>
2021-07-28 18:40:48 -0500

    next.config.js working w expo 42

--- next.config.js
+++ next.config.js
@@ -1,8 +1,21 @@
 // @generated: @expo/next-adapter@2.1.9
 // Learn more: https://github.com/expo/expo/blob/master/docs/pages/guides/using-nextjs.md
 
-const { withExpo } = require('@expo/next-adapter')
+const { withExpo } = require("@expo/next-adapter");
+const withPlugins = require("next-compose-plugins");
+const withTM = require("next-transpile-modules")(["react-native-web"]);
 
-module.exports = withExpo({
-  projectRoot: __dirname,
-})
+const nextConfig = {
+  distDir: "build",
+  reactStrictMode: true,
+  webpack: (config, options) => {
+    // modify the `config` here
+
+    return config;
+  },
+};
+
+module.exports = withPlugins(
+  [withTM, [withExpo, { projectRoot: __dirname }]],
+  nextConfig
+);
